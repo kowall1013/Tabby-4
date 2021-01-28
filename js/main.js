a3bd9bf4f64ae665bc0e744d0891e1b8e55a8cf3
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-01-28 06:41:24 +0100

    feat: add event delegation pattern

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const tabs = Array.from(document.querySelectorAll('.tab'))
 const tabby = document.querySelector('.tabby')
 const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
+const tabsContainer = tabby.querySelector('.tabs')
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', event => {
-    const target = tab.dataset.theme
-    const tabContent = tabby.querySelector('#' + target)
-    tabs.forEach(item => item.classList.remove('is-selected'))
-    tab.classList.add('is-selected')
-    tabContents.forEach(content => content.classList.remove('is-selected'))
-    tabContent.classList.add('is-selected')
+const handleTabs = e => {
+  const tab = e.target
+  const target = tab.dataset.theme
+  const tabContent = tabby.querySelector('#' + target)
+  tabs.forEach(item => item.classList.remove('is-selected'))
+  tab.classList.add('is-selected')
+  tabContents.forEach(content => content.classList.remove('is-selected'))
+  tabContent.classList.add('is-selected')
+}
 
-  } )
-})
+tabsContainer.addEventListener('click', handleTabs)
+
